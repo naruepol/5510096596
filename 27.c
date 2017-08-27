95e19a2d22765867edcebdd4ae9b8c08347c15a7
naruepol <naruapon@gmail.com>
2017-08-27 15:22:39 +0700

    แก้ไขคำสั่ง บรรทัดที่ 4

--- 27.c
+++ 27.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	printf("123467890123467890123467890");
+	printf("123456789012345678901234567890");
 	printf("\n%20d*",46);
 	printf("\n%-20d*",46);
 	printf("\n%3d*",46);
