79fe089a79b43eeb3adee79034b3b3aac0f24cdb
Naruapon <naruapon@gmail.com>
2017-09-17 14:29:26 +0700

    add 3.27.c

---
+++ 3-27.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	char name[20] = "Kmitl";
+	
+	printf("%s\n",name);
+	printf("%s\n",name);
+	printf("%s\n",name);
+	printf("%s\n",name);
+	printf("%s\n",name);
+	return 0;	
+}
