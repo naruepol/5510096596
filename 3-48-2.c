62849f23fba6c67df4ae61f0fea78ac89f2576a1
naruapon <naruapon@webmail.npru.ac.th>
2017-09-24 15:08:24 +0700

    add 3-48-2

---
+++ 3-48-2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	
+	puts("Business");
+	puts("\n");
+	
+	char s1[] = "Computer";
+	puts(s1);
+	puts("\n");
+	
+	printf("%s","University");
+	
+	return 0;
+}
--- 3-48.c
+++ 3-48.c
@@ -2,9 +2,11 @@
 int main(){
 	
 	putchar('A');
+	putchar('\n');
 	
 	char ch1 = 'B';
 	putchar(ch1);
+	putchar('\n');
 	
 	printf("%c",'C');
 	
